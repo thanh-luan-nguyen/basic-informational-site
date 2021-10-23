3fb63d2547f55fc8549389e05cd889f6c5d9016b
NGUYEN THANH LUAN <luanconsutorakushon@gmail.com>
2021-10-23 20:30:04 +0900

    .

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 var fs = require('fs');
 require('dotenv').config();
 var port = process.env.PORT;
 app.get('/', function (req, res) {
-    fs.readFile('index.html', function (e, html) {
-        if (e) {
-            console.log(e);
-            return;
-        }
-        res.send(html);
-    });
+    res.sendFile(path.join(__dirname, '/index.html'));
+});
+app.get('/about', function (req, res) {
+    res.sendFile(path.join(__dirname, '/about.html'));
+});
+app.get('/contact', function (req, res) {
+    res.sendFile(path.join(__dirname, '/contact.html'));
+});
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '/404.html'));
 });
 app.listen(port, function () {
     console.log("Listening on port " + port + "...");
--- index.ts
+++ index.ts
@@ -1,17 +1,21 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const fs = require('fs')
 require('dotenv').config()
 const port = process.env.PORT
 
 app.get('/', (req, res) => {
-  fs.readFile('index.html', (e, html) => {
-    if (e) {
-      console.log(e)
-      return
-    }
-    res.send(html)
-  })
+  res.sendFile(path.join(__dirname, '/index.html'))
+})
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, '/about.html'))
+})
+app.get('/contact', (req, res) => {
+  res.sendFile(path.join(__dirname, '/contact.html'))
+})
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/404.html'))
 })
 
 app.listen(port, () => {

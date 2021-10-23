efa7a2a3b5e0f636ce2b375dab6f198e09261cdc
NGUYEN THANH LUAN <luanconsutorakushon@gmail.com>
2021-10-23 20:15:52 +0900

    .

--- index.js
+++ index.js
@@ -1,37 +1,17 @@
-var http = require('http');
+var express = require('express');
+var app = express();
 var fs = require('fs');
 require('dotenv').config();
 var port = process.env.PORT;
-function getFilePath(reqURL) {
-    var filePath;
-    switch (reqURL) {
-        case '/':
-            filePath = 'index.html';
-            break;
-        case '/about':
-            filePath = 'about.html';
-            break;
-        case '/contact':
-            filePath = 'contact.html';
-            break;
-        default:
-            filePath = '404.html';
-    }
-    return filePath;
-}
-var server = http.createServer(function (req, res) {
-    var filePath = getFilePath(req.url);
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    fs.readFile(filePath, function (e, html) {
+app.get('/', function (req, res) {
+    fs.readFile('index.html', function (e, html) {
         if (e) {
             console.log(e);
             return;
         }
-        res.write(html);
-        res.end();
+        res.send(html);
     });
 });
-server.listen(port, function () {
-    console.log("Server running at port " + port + "...");
+app.listen(port, function () {
+    console.log("Listening on port " + port + "...");
 });
--- index.ts
+++ index.ts
@@ -1,41 +1,19 @@
-const http = require('http')
+const express = require('express')
+const app = express()
 const fs = require('fs')
 require('dotenv').config()
-
 const port = process.env.PORT
 
-function getFilePath(reqURL) {
-  let filePath
-  switch (reqURL) {
-    case '/':
-      filePath = 'index.html'
-      break
-    case '/about':
-      filePath = 'about.html'
-      break
-    case '/contact':
-      filePath = 'contact.html'
-      break
-    default:
-      filePath = '404.html'
-  }
-  return filePath
-}
-
-const server = http.createServer((req, res) => {
-  const filePath = getFilePath(req.url)
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text/html')
-  fs.readFile(filePath, (e, html) => {
+app.get('/', (req, res) => {
+  fs.readFile('index.html', (e, html) => {
     if (e) {
       console.log(e)
       return
     }
-    res.write(html)
-    res.end()
+    res.send(html)
   })
 })
 
-server.listen(port, () => {
-  console.log(`Server running at port ${port}...`)
+app.listen(port, () => {
+  console.log(`Listening on port ${port}...`)
 })
